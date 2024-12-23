e0fecf64c8c2cec53bf75fd0eeec4b4bf95bc84e
Thomas Engels <m.darkbasicer@gmail.com>
2024-12-23 06:43:42 +0100

    added StructureManager for building platforms and props

---
+++ Leafy Life/Assets/Scripts/Structure.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Structure : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public bool isWalkable;
+    public bool isInteractable;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
