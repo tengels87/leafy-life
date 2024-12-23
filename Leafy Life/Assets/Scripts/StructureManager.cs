8e12678676400a54b66910db25513099a6824056
Thomas Engels <m.darkbasicer@gmail.com>
2024-12-23 06:44:26 +0100

    added simple test routine to StructureManager

--- Leafy Life/Assets/Scripts/StructureManager.cs
+++ Leafy Life/Assets/Scripts/StructureManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class StructureManager : MonoBehaviour
 {
+    public GameObject prefab_platform;
+
     void Start()
     {
         
@@ -11,6 +13,24 @@ public class StructureManager : MonoBehaviour
 
     void Update()
     {
-        
+        // Test
+        if (Input.GetMouseButtonDown(0)) {
+            MapController mapController = WorldConstants.Instance.getMapController();
+
+            Vector2 pp = MapController.pixelPos2WorldPos(Input.mousePosition);
+
+            if (mapController.isEmpty(new Vector2Int((int)pp.x, (int)pp.y))) {
+
+                // instantiate
+                GameObject platform = (GameObject)Object.Instantiate(prefab_platform);
+                platform.transform.position = new Vector3((int)pp.x, (int)pp.y, 0);
+                //goHeart.transform.SetParent(spriteContainer);
+
+                MapController.Tile t = new MapController.Tile((int)pp.x, (int)pp.y, platform);
+                
+                mapController.spawnTile(t);
+
+            }
+        }
     }
 }
