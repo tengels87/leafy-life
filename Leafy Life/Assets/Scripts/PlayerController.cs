322256b3dd44a9cad90d3ffe92fa650a4ca609fa
Thomas Engels <m.darkbasicer@gmail.com>
2025-01-01 07:29:01 +0100

    added animations to player
    - animation controller
    - animations
    - gfx

--- Leafy Life/Assets/Scripts/PlayerController.cs
+++ Leafy Life/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public float movementSpeed = 1f;
+
+    private Vector3 moveDirection;
     private List<Vector2> waypointList = new List<Vector2>();
     private List<Vector2> targetList = new List<Vector2>();
     private bool canMove = true;
 
+    private SpriteRenderer spriteRenderer;
+    private Animator anim;
+
     void Start() {
         this.transform.position = new Vector3(4, 4, 0);
+
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+        anim = this.GetComponent<Animator>();
     }
 
     void Update() {
@@ -41,6 +50,18 @@ public class PlayerController : MonoBehaviour {
 
         // walk along current path
         navigateAllWaypoints();
+
+        // flip sprite according to movementDirection
+        Vector3 scale = spriteRenderer.transform.localScale;
+        if (Mathf.Abs(moveDirection.x) > Mathf.Abs(moveDirection.y)) {
+
+            scale.x = 0.1f * moveDirection.x;
+            anim.SetBool("isClimbing", false);
+        } else if (Mathf.Abs(moveDirection.x) < Mathf.Abs(moveDirection.y)) {
+            scale.x = 0.1f;
+            anim.SetBool("isClimbing", true);
+        }
+            spriteRenderer.transform.localScale = scale;
     }
 
     public Vector2 getPosition() {
@@ -81,17 +102,21 @@ public class PlayerController : MonoBehaviour {
 
     public void navigateAllWaypoints() {
         if (waypointList.Count > 0) {
+            anim.SetFloat("speed", 1f);
+
             Vector2 targetWaypoint = waypointList[0];
             float dist = Vector2.Distance(targetWaypoint, getPosition());
             if (dist > 0.1f) {
                 if (canMove) {
-                    Vector3 moveDIr = (targetWaypoint - getPosition()).normalized;
+                    moveDirection = (targetWaypoint - getPosition()).normalized;
 
-                    this.transform.position = transform.position + moveDIr * 4 * Time.deltaTime;
+                    this.transform.position = this.transform.position + moveDirection * movementSpeed * Time.deltaTime;
                 }
             } else {
                 waypointList.RemoveAt(0);
             }
+        } else {
+            anim.SetFloat("speed", 0);
         }
     }
 
