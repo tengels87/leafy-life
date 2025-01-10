7f2d3c948e9dbc8556af7c3242b44959a536fdc2
Thomas Engels <m.darkbasicer@gmail.com>
2025-01-10 11:19:04 +0100

    auto arrange HUD

--- Leafy Life/Assets/Scripts/HudManager.cs
+++ Leafy Life/Assets/Scripts/HudManager.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class HudManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject resourcesPanel;
+    public GameObject buildPanel;
+
     void Start()
     {
-        
+        Camera cam = Camera.main;
+
+        MapController mapController = WorldConstants.Instance.getMapController();
+        mapController.updateCameraFOV(8, 11);
+
+        float visibleWorldWidth = 2 * cam.orthographicSize * cam.aspect;
+
+        resourcesPanel.transform.localPosition = new Vector3(-0.5f * visibleWorldWidth, cam.orthographicSize, 10);
+        buildPanel.transform.localPosition = new Vector3(-0.5f * visibleWorldWidth, cam.orthographicSize, 10);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
