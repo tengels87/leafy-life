f266e9c483736f79a75b0196b8e04965430391d6
Thomas Engels <m.darkbasicer@gmail.com>
2025-11-18 22:46:37 +0100

    activate build slots based on map type
    * i.e., digging only in GARDEN, platforms only in TREEHOUSE

--- Leafy Life/Assets/Scripts/BuildController.cs
+++ Leafy Life/Assets/Scripts/BuildController.cs
@@ -8,24 +8,18 @@ public class BuildController : DragInteractController
 {
     public static UnityAction<ItemData> OnCollectedEvent;
 
+    public MapController.MapType activeInMapType;
     public PrefabDef buildPrefabDef;
     public ItemData buildIngredients;
     public bool ignoreIngredients;
 
-    private bool isEnabled = false;
-
     private void Awake() {
-        setEnabled(canBuild());
-    }
+        setActive(canBuild());
 
-    private void OnEnable() {
         Inventory.ItemAddedEvent += OnItemAdded;
         Inventory.ItemRemovedEvent += OnItemRemoved;
-    }
 
-    private void OnDisable() {
-        Inventory.ItemAddedEvent += OnItemAdded;
-        Inventory.ItemRemovedEvent += OnItemRemoved;
+        SceneManager.MapChangedEvent += OnMapChanged;
     }
 
     protected override void Start()
@@ -64,17 +58,11 @@ public class BuildController : DragInteractController
         }
     }
 
-    private void OnItemAdded(ItemData item, bool isFirstOfThisKind) {
-        setEnabled(canBuild());
-    }
-
-    private void OnItemRemoved(ItemData item, bool isFirstOfThisKind) {
-        setEnabled(canBuild());
-    }
-
     private void setEnabled(bool val) {
-        isEnabled = val;
+        this.gameObject.SetActive(val);
+    }
 
+    private void setActive(bool val) {
         // set disabled state via material shader
         Image icon = this.gameObject.GetComponent<Image>();
         if (icon != null) {
@@ -93,5 +81,17 @@ public class BuildController : DragInteractController
         }
 
         return false;
+    }
+
+    private void OnItemAdded(ItemData item, bool isFirstOfThisKind) {
+        setActive(canBuild());
+    }
+
+    private void OnItemRemoved(ItemData item, bool isFirstOfThisKind) {
+        setActive(canBuild());
+    }
+
+    private void OnMapChanged(MapController.MapType mapType) {
+        setEnabled(mapType == activeInMapType);
     }
 }
